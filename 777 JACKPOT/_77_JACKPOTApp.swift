fe809e50208931147ea5d282c020fc672574f530
DAndyPhm <s3818286@rmit.edu.vn>
2022-08-19 13:07:01 +0700

    Initial Commit

---
+++ 777 JACKPOT/_77_JACKPOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  _77_JACKPOTApp.swift
+//  777 JACKPOT
+//
+//  Created by An Pham Hoang Thien on 19/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct _77_JACKPOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
